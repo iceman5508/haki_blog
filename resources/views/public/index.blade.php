4e4919b64c8b1ef1cfe1deb93d4c0f4d48ac835e
isaac parker <isaacparker94@gmail.com>
2023-12-18 00:08:34 -0500

    "Gallery"

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -82,6 +82,10 @@
                         </li>
                         <li class="mr-2">
                             <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:underline py-2 px-2"
+                               href="{{'gallery'}}">Gallery</a>
+                        </li>
+                        <li class="mr-2">
+                            <a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:underline py-2 px-2"
                                href="{{route('login.form')}}">Admin</a>
                         </li>
                     </ul>
